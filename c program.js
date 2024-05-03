3fc36e24493e614d08801595081f75ea7142c0dc
vansh020 <vanshdhalor04@gmail.com>
2024-05-03 14:48:35 +0530

    first commit

---
+++ c program.js
@@ -0,0 +1,63 @@
+ // Function to set the mode
+ document.addEventListener('DOMContentLoaded', function() {
+    var savedMode = localStorage.getItem('selectedMode');
+
+    if (savedMode) {
+        setMode(savedMode);
+        document.getElementById('mode-toggle').checked = savedMode === 'dark';
+    } 
+    else {
+        setMode('light'); // Default mode
+    }
+
+    document.getElementById('mode-toggle').addEventListener('change', function() {
+        var selectedMode = this.checked ? 'dark' : 'light';
+        localStorage.setItem('selectedMode', selectedMode);
+        setMode(selectedMode);
+    });
+});
+
+function setMode(mode) {
+    var body = document.body;
+    var lightModeImage = document.querySelector('.light-mode-img');
+    var darkModeImage = document.querySelector('.dark-mode-img');
+
+    if (mode === 'light') {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+        if (lightModeImage) {
+            lightModeImage.style.display = 'inline';
+        }
+        if (darkModeImage) {
+            darkModeImage.style.display = 'none';
+        }
+    } else if (mode === 'dark') {
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+        if (lightModeImage) {
+            lightModeImage.style.display = 'none';
+        }
+        if (darkModeImage) {
+            darkModeImage.style.display = 'inline';
+        }
+    }
+}
+
+// to prevent
+window.onload = function() {
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
+};
+
+
+// side nav for js
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+ 
+  }
+ 
